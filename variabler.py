74ada903d5865c53a3a207ade0512f0704a807ab
sukhman112 <70263858+sukhman112@users.noreply.github.com>
2020-09-02 11:38:03 +0200

    varaibal

---
+++ variabler.py
@@ -0,0 +1,11 @@
+name = "sukhman"
+age = 17
+print("hej jag heter sukhman och är 17 år gammal")
+
+sida = float(input("ange kvadratens sida:"))
+
+area = sida**2
+
+omkrets = 4*sida
+
+print(f"kvadratens area är {area} a.e och kvadratens omkrets är ")
